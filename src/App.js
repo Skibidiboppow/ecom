7e9a0de3dc7c454e4467ffe1a6210e2739c42930
ruprabh <ruprabh@gmail.com>
2024-03-11 23:25:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Products from './components/Products/Products';
 import Cart from './components/Cart/Cart';
 
 function App() {
-  //Is it working
+
   const [showCart, setShowCart]= useState(false);
   const [cartItems,setCartItems] = useState([]);
   
@@ -12,17 +12,61 @@ function App() {
   const closeCart = () => setShowCart(false);
 
   const handleAddToCart = (productId,productName,productImage)=>{
-    
-    console.log(productName);
-    
+    const productInCartIndex = cartItems.findIndex(item => item.id === productId
+      );
+    if (productInCartIndex === -1) {
+      
+      const newCartItem ={
+        id:productId,
+        name:productName,
+        image:productImage,
+        quantity:1
+      }; 
+      // setCartItems([...cartItems,newCartItem]);  
+      // setCartItems(cartItems.concat(newCartItem)); 
+      setCartItems((state)=>[...state,newCartItem]);
+    }else{
+      let updateCartItem = [...cartItems];
+      updateCartItem[productInCartIndex].quantity+=1;
+      setCartItems(updateCartItem);
+    }
   };
+  const handleIncreaseQuantity = (productId)=>{
+    const productInCartIndex = cartItems.findIndex(item => item.id === productId
+      );
+      let updateCartItem = [...cartItems];
+
+
+      updateCartItem[productInCartIndex].quantity+=1;
+      setCartItems(updateCartItem);
+
+  }
+  const handleDecreaseQuantity = (productId)=>{
+    const productInCartIndex = cartItems.findIndex(item => item.id === productId
+      );
+      const qty =cartItems[productInCartIndex].quantity;
+      let updateCartItem = [...cartItems];
+      if(qty === 1){
+        updateCartItem = updateCartItem.filter((item)=> item.id !== productId )
+      }else{
+        updateCartItem[productInCartIndex].quantity-=1;
+      }
+      setCartItems(updateCartItem);
+
+  }
 
   return (
     <div >
       <Header openCart={openCart}/>
       <Products onAddToCart={handleAddToCart}/>
-      <Cart showCart={showCart} closeCart={closeCart} />
-      Hello from app
+      <Cart 
+      showCart={showCart} 
+      closeCart={closeCart} 
+      cartItems={cartItems} 
+      onIncQuantity = {handleIncreaseQuantity}
+      onDecQuantity = {handleDecreaseQuantity}
+      />
+
     </div>
   );
 }
